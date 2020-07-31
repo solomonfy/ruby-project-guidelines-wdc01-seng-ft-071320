8945ed025c2f0f94b3f2fc0a5e909bdd4a8539f4
solomonfy <fantahunsolomon@gmail.com>
2020-07-31 04:36:53 -0400

    CLI

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -74,16 +74,16 @@ def print_menu
     menu = Artii::Base.new
     puts Rainbow(menu.asciify("Menu - choose an option")).green
     puts ""
-    puts (" 1. ") + "Buy a car from manufacturer"
-    puts (" 2. ") + "Sell a car to a buyer"
-    puts (" 3. ") + "Check inventory"
-    puts (" 4. ") + "See vehicle information"
-    puts (" 5. ") + "Check account balance"
-    puts (" 6. ") + "Current stock and total stock cost"
-    puts (" 7. ") + "Oldest vehicle"
-    puts (" 8. ") + "Change vehicle price"
-    puts (" 9. ") + "All suppliers(manufacturers)"
-    puts (" 10. ") + "Exit"
+    puts ("             1. ") + "Buy a car from manufacturer"
+    puts ("             2. ") + "Sell a car to a buyer"
+    puts ("             3. ") + "Check inventory"
+    puts ("             4. ") + "See vehicle information"
+    puts ("             5. ") + "Check account balance"
+    puts ("             6. ") + "Current stock and total stock cost"
+    puts ("             7. ") + "Oldest vehicle"
+    puts ("             8. ") + "Change vehicle price"
+    puts ("             9. ") + "All suppliers(manufacturers)"
+    puts ("             10. ") + "Exit"
     puts ""
     input = gets.chomp
 end
@@ -91,7 +91,7 @@ end
 # Helper methods for buy a car from manufacturer
 
 def select_manufacturer
-    puts "\n\nSelect a manufacturer: "
+    puts "\nSelect a manufacturer: "
     puts Manufacturer.manufacturer_list
     @manufacturer_index = gets.chomp.to_i
     if !@manufacturer_index.between?(1, Manufacturer.all.count)
@@ -105,7 +105,7 @@ def get_a_manufacturer
 end
 
 def select_vehicle
-    puts "\n\nChoose vehicle number:"
+    puts "\nChoose vehicle number:"
     puts @this_manufacturer.manufacturer_inventory_list
     @man_vehicle_index = gets.chomp.to_i
     if !@man_vehicle_index.between?(1, @this_manufacturer.vehicles.count)
@@ -182,14 +182,18 @@ def menu_nav(input)
                 puts "Vehicle specifications: year - #{@this_vehicle.year}, make - #{@this_vehicle.make}, model number - #{@this_vehicle.model}"
                 puts "Selling price: #{@this_vehicle.dealer_selling_price}\n"
             else
-                puts "\nYou don't have a vehicle to sell, please stock a vehicle from your suppliers!"
+                puts Rainbow("\nYou don't have a vehicle to sell, please stock a vehicle from your suppliers!").red
             end
         when "3"
             @this_dealer.inventory_list
         when "4"
-            select_vehicle_from_dealer
-            get_vehicle_from_dealer
-            @this_vehicle.all_info
+            if @this_dealer.vehicles.count > 0
+                select_vehicle_from_dealer
+                get_vehicle_from_dealer
+                @this_vehicle.all_info
+            else
+                puts Rainbow("You don't have vehicle in stock, please buy vehicle(s) from your suppliers!").red
+            end
         when "5"
             puts "Your current balance is $#{@this_dealer.account_balance}"
         when "6"
@@ -205,11 +209,12 @@ def menu_nav(input)
             change_vehicle_price(@this_vehicle, @new_price)
             
         when "9"
+            puts "List of supplier(s):"
             Manufacturer.manufacturer_list
         when "10"
             goodbye = Artii::Base.new
             puts Rainbow(goodbye.asciify("Good bye  !")).red
         else
-          puts "That option does not exist. Please select again"
+          puts Rainbow("That option does not exist. Please try again!").yellow.bold
       end
 end
